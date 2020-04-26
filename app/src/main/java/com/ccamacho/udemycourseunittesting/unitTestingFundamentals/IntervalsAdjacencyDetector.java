beebcda9459bbd2c595435b1e1e6b5852a20dfa4
conrrado <con.webmaster@gmail.com>
2020-04-26 16:08:45 -0300

    implementação do método isAdjacency para satisfazer os testes da classe IntervalsAdjacencyDetector

--- app/src/main/java/com/ccamacho/udemycourseunittesting/unitTestingFundamentals/IntervalsAdjacencyDetector.java
+++ app/src/main/java/com/ccamacho/udemycourseunittesting/unitTestingFundamentals/IntervalsAdjacencyDetector.java
@@ -3,6 +3,6 @@ package com.ccamacho.udemycourseunittesting.unitTestingFundamentals;
 public class IntervalsAdjacencyDetector {
 
     public boolean isAdjacency(Interval interval1, Interval interval2) {
-        return false;
+        return interval1.getEnd() == interval2.getStart() || interval1.getStart() == interval2.getEnd();
     }
 }
